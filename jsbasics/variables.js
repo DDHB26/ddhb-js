bdebb7560ff9d5267b858437b71075b4e7f1cd7e
DDHB26 <168258796+DDHB26@users.noreply.github.com>
2024-06-19 16:49:56 +0000

    variables done

---
+++ jsbasics/variables.js
@@ -0,0 +1,8 @@
+let mvpname="bhajnit singh hooda";
+const mvpno=1;              //const keyword lagake dekho varibale ka colour alag hie hojate kyuki aage program mai ye variable ki value change nhi hogi
+mvpid=26;                   //bad practice to not decalre variable without let or const but we can do it
+let best="ddhb"
+let bestno=1
+let definedbe
+
+console.table([mvpname,mvpno,mvpid,best,bestno])
