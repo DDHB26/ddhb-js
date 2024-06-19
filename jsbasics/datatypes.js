b9c0a0779046d0e7208c6de62db37823e0b4afa1
DDHB26 <168258796+DDHB26@users.noreply.github.com>
2024-06-19 21:07:31 +0000

    datatypes

---
+++ jsbasics/datatypes.js
@@ -0,0 +1,28 @@
+//null and undefined are also datatypes along with the string int bigint boolean
+//but when we ask type of null it says object 
+//symbol is datatype used unique stuffs
+//number is also a data type here gaur farmaiye on output
+//in actual use we write  all data types in Number String this format like using for Classes
+// "use strict" //used to treate all js code as newer version // alert() is not used now as js is nomore only applicable till browser now we can also do dsa on it
+// console.log(typeof "baksish")
+// console.log(typeof 8); console.log(typeof null); console.log(typeof undefined)
+
+//coversion
+
+let random="3";
+let converttonumber=Number(random);
+console.log(typeof random);
+console.log(typeof converttonumber);
+random="33abc";
+converttonumber=Number(random);
+console.log(typeof random);
+console.log(typeof converttonumber);
+console.log(converttonumber) //as 33abc is not a number hence o/p NaN
+random=null;
+converttonumber=Number(null);
+console.log(typeof random);
+console.log(typeof converttonumber);
+console.log(converttonumber) //null ka number 0 aur undefined ka number NaN // true ka number 1 aur false ka zero
+
+
+
