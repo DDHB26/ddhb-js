a11e4d18f9828d4fc3f2484ad1cdd0fc87a8bb42
DDHB26 <168258796+DDHB26@users.noreply.github.com>
2024-07-08 13:07:54 +0000

    bmi

--- project/project2.js
+++ project/project2.js
@@ -14,17 +14,17 @@ form.addEventListener('submit',function(event){
     }
     else{
       const bmi= (weight/((height*height)/10000)).toFixed(2);
-      const nicewt1=(((24.9*(height*height))/10000)).toFixed(2)
+      const nicewt=(((24.9*(height*height))/10000)).toFixed(2)
       const nicewt2=(((18.6*(height*height))/10000)+1).toFixed(2)
       if(bmi<18.6){
-          results.innerHTML=`thoda khana kha bhai, ${bmi} itna kum bmi , ${nicewt2} to ${nicewt1} is a good range for you`
+          results.innerHTML=`thoda khana kha bhai, ${bmi} itna kum bmi , ${nicewt2} to ${nicewt} is good range for you , so minimum inc in wt =${nicewt2-weight} kg and maximum- ${nicewt-weight}kg`
 
       }
       else if(bmi<24.9){
           results.innerHTML=`fit person with bmi ${bmi}`
       }
       else{
-        results.innerHTML=`Heavy weight champion with ${bmi} bmi, reduce till ${nicewt1} weight`
+        results.innerHTML=`Heavy weight champion with ${bmi} bmi, reduce till ${nicewt} weight means reduce ${weight-nicewt} kg`
       }
 
     }
